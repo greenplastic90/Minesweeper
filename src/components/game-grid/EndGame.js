a2a7854123ea5ff6f06899bd3f74d98043e0b704
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2022-12-06 19:37:47 +0300

    EndGame animates when showed

--- src/components/game-grid/EndGame.js
+++ src/components/game-grid/EndGame.js
@@ -1,12 +1,13 @@
 import { Button, HStack, Text, VStack } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 import React from 'react'
 import { ImClock2, ImTrophy } from 'react-icons/im'
 import { BsArrowClockwise } from 'react-icons/bs'
 
 const EndGame = ({ resetGame }) => {
 	return (
-		<VStack pos={'absolute'} w='full' h='full'>
-			<VStack w='200px' zIndex={3} pt={['20px', null, '10%']}>
+		<VStack pos={'absolute'} w='full' h='full' bg={'hsla(100, 100%, 10%, 0.3)'}>
+			<VStack as={motion.div} animate={{ scale: [0, 1], transition: { delay: 0.75 } }} w='200px' zIndex={3} pt={['20px', null, '10%']}>
 				<Times />
 				<Button w='full' onClick={resetGame} borderRadius={5} _hover={{ bg: '#243E14' }} bgColor='brand.header' color='brand.header_text'>
 					<BsArrowClockwise onClick={resetGame} style={{ transform: 'rotate(60deg)' }} size={'20px'} /> <Text pl={'15px'}>Play Again</Text>
