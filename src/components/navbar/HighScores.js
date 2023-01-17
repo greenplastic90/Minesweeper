20757f92992a88dc107c422a4d5a11b5d861fe2c
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-01-17 17:57:09 +0300

    Instructions and High Scores are buttons

--- src/components/navbar/HighScores.js
+++ src/components/navbar/HighScores.js
@@ -1,7 +1,12 @@
+import { Button } from '@chakra-ui/react'
 import React from 'react'
 
 const HighScores = () => {
-	return <div>HighScores</div>
+	return (
+		<Button fontWeight={'normal'} borderRadius={0}>
+			High Scores
+		</Button>
+	)
 }
 
 export default HighScores
--- src/components/navbar/HowToPlay.js
+++ src/components/navbar/HowToPlay.js
@@ -1,7 +1,12 @@
+import { Button } from '@chakra-ui/react'
 import React from 'react'
 
 const HowToPlay = () => {
-	return <div>HowToPlay</div>
+	return (
+		<Button fontWeight={'normal'} borderRadius={0}>
+			Instructions
+		</Button>
+	)
 }
 
 export default HowToPlay
