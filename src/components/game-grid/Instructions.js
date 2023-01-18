6f3acc4fa1e9b2b9de9bb42179ef1e290fd5d4af
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-01-18 15:10:44 +0300

    setInterval to toggle between Dig and Flag instructions

--- src/components/game-grid/Instructions.js
+++ src/components/game-grid/Instructions.js
@@ -1,8 +1,28 @@
 import { VStack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 
 const Instructions = () => {
-	return <VStack pos={'absolute'} top={'20%'} w='200px' h='150px' mt={0} bg={'hsla(100, 100%, 10%, 0.6)'}></VStack>
+	const [showDig, setShowDig] = useState(true)
+
+	useEffect(() => {
+		setInterval(() => {
+			setShowDig((current) => !current)
+		}, 1000 * 2)
+	}, [])
+	return (
+		<VStack pointerEvents={'none'} pos={'absolute'} top={'20%'} w='200px' h='150px' mt={0} bg={'hsla(100, 100%, 10%, 0.6)'}>
+			{showDig ? <Dig /> : <Flag />}
+		</VStack>
+	)
 }
 
 export default Instructions
+
+const Dig = () => {
+	return <div>Dig</div>
+}
+
+const Flag = () => {
+	return <div>Flag</div>
+}
