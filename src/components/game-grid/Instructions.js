c57154d7f1ff359a910005a112d48cedda30feb9
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-01-18 18:11:52 +0300

    refactored Instructions to be easier to read and update

--- src/components/game-grid/Instructions.js
+++ src/components/game-grid/Instructions.js
@@ -1,5 +1,5 @@
-import { HStack, Text, VStack } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import { HStack, Stack, Text, VStack } from '@chakra-ui/react'
+import React, { useRef, useState } from 'react'
 import { useEffect } from 'react'
 import { basicColors } from '../../theme/theme'
 import { isMobileOrTablet } from './grid-functions'
@@ -7,61 +7,63 @@ import { isMobileOrTablet } from './grid-functions'
 import { TbShovel } from 'react-icons/tb'
 import { RiFlag2Fill } from 'react-icons/ri'
 import { BsMouse2 } from 'react-icons/bs'
-import { FaRegHandPointer } from 'react-icons/fa'
+import { FaRegHandPointDown } from 'react-icons/fa'
+
+const iconsStyle = { color: basicColors.white, size: '50px' }
 
 const Instructions = () => {
 	const [showDig, setShowDig] = useState(true)
+	const [instructions, setInstructions] = useState()
+
+	const instructionData = useRef({
+		mobile: {
+			icon: <FaRegHandPointDown color={iconsStyle.color} size={iconsStyle.size} />,
+			text: {
+				digText: 'PRESS',
+				flagText: 'LONG PRESS',
+			},
+		},
+
+		browser: {
+			icon: <BsMouse2 color={iconsStyle.color} size={iconsStyle.size} />,
+			text: {
+				digText: 'LEFT CLICK',
+				flagText: 'RIGHT CLICK',
+			},
+		},
+	})
 
 	useEffect(() => {
+		setInstructions(isMobileOrTablet() ? instructionData.current.mobile : instructionData.current.browser)
 		setInterval(() => {
 			setShowDig((current) => !current)
 		}, 1000 * 3)
 	}, [])
+
 	return (
-		<VStack pointerEvents={'none'} pos={'absolute'} top={'20%'} w='200px' h='150px' mt={0} justifyContent={'center'} borderRadius={'20px'} bg={'hsla(0, 0%, 0%, 0.8)'}>
-			{showDig ? <Dig /> : <Flag />}
-		</VStack>
+		<>
+			{instructions && (
+				<VStack pointerEvents={'none'} pos={'absolute'} top={'20%'} w='200px' h='150px' mt={0} justifyContent={'center'} borderRadius={'20px'} bg={'hsla(0, 0%, 0%, 0.8)'}>
+					{showDig ? <DisplayInformation data={instructions} isDig={true} /> : <DisplayInformation data={instructions} isDig={false} />}
+				</VStack>
+			)}
+		</>
 	)
 }
 
 export default Instructions
 
-const Dig = () => {
+const DisplayInformation = ({ data, isDig }) => {
+	const { icon, text } = data
+	const { digText, flagText } = text
 	return (
-		<InstructionsContainer>
-			<IconsContainer>
-				<TbShovel color={basicColors.white} size={'50px'} />
-				{isMobileOrTablet() ? <FaRegHandPointer color={basicColors.white} size={'50px'} /> : <BsMouse2 color={basicColors.white} size={'50px'} />}
-			</IconsContainer>
-			<TextContainer>{isMobileOrTablet() ? 'PRESS' : 'LEFT CLICK'}</TextContainer>
-		</InstructionsContainer>
-	)
-}
-
-const Flag = () => {
-	return (
-		<InstructionsContainer>
-			<IconsContainer>
-				{isMobileOrTablet() ? <FaRegHandPointer color={basicColors.white} size={'50px'} /> : <BsMouse2 color={basicColors.white} size={'50px'} />}
-				<RiFlag2Fill color={basicColors.white} size={'50px'} />
-			</IconsContainer>
-			<TextContainer>{isMobileOrTablet() ? 'LONG PRESS' : 'RIGHT CLICK'}</TextContainer>
-		</InstructionsContainer>
-	)
-}
-
-const InstructionsContainer = ({ children }) => {
-	return <VStack>{children}</VStack>
-}
-
-const IconsContainer = ({ children }) => {
-	return <HStack>{children}</HStack>
-}
-
-const TextContainer = ({ children }) => {
-	return (
-		<Text fontSize={'30px'} color={'gray.100'}>
-			{children}
-		</Text>
+		<VStack>
+			<Stack direction={isDig ? 'row' : 'row-reverse'}>
+				{isDig ? <TbShovel color={iconsStyle.color} size={iconsStyle.size} /> : <RiFlag2Fill color={iconsStyle.color} size={iconsStyle.size} />} {icon}{' '}
+			</Stack>
+			<Text fontSize={'30px'} color={'gray.100'}>
+				{isDig ? digText : flagText}
+			</Text>
+		</VStack>
 	)
 }
