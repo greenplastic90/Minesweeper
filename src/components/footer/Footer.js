475003995e49c3149be28a2dc3a1b3622074d82f
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-01-26 13:53:48 +0300

    added social icons and links to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,35 @@
-import { HStack, Text } from '@chakra-ui/react'
+import { HStack, Link } from '@chakra-ui/react'
 import React from 'react'
-import { basicColors } from '../../theme/theme'
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
 
 const Footer = () => {
+	const sociaslAtt = { iconSize: '30px', color: 'white' }
+	const socials = [
+		{
+			icon: <FaGithubSquare size={sociaslAtt.iconSize} color={sociaslAtt.color} />,
+			link: 'https://github.com/greenplastic90/Minesweeper',
+		},
+		{
+			icon: <FaLinkedin size={sociaslAtt.iconSize} color={sociaslAtt.color} />,
+			link: 'https://www.linkedin.com/in/bashar-othman/',
+		},
+	]
+
 	return (
-		<HStack w={'full'} justifyContent={'flex-end'} h='30px'>
-			<Text fontWeight={'bold'} fontSize={'30px'} color={basicColors.blue_dark}>
-				BasharOthman
-			</Text>
+		<HStack w={'full'} justifyContent={'center'} pb={'20px'}>
+			{socials.map((social) => (
+				<Social key={social.link} icon={social.icon} link={social.link} />
+			))}
 		</HStack>
 	)
 }
 
 export default Footer
+
+const Social = ({ icon, link }) => {
+	return (
+		<Link href={link} isExternal>
+			{icon}
+		</Link>
+	)
+}
